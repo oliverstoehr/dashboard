7f50bfd2d0c3e4076df637cc0b65ee56d8fd9544
Oliver Stöhr <oliver.alexander.stoehr@stud.fh-luebeck.de>
2017-08-31 11:46:28 +0200

    created index.php file, included bootstrap and chart.js

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Dashboard</title>
+		<!-- including Bootstrap CSS -->
+		<link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+	</head>
+	<body>
+		<!-- Header -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+			<span class="navbar-brand">Dashboard</span>
+			<button id="refresh" class="btn btn-outline-success my-2 my-sm-0" type="button">refresh</button>
+		</nav>
+		<!-- Main content -->
+		<main>
+			
+		</main>
+		
+		<!-- including chart.js -->
+		<script src="chartjs/chart.bundle.js"></script>
+	</body>
+</html>
