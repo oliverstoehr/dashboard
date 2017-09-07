911d330952fa5f972f6edb6857b2fe9a73539cc1
Oliver Stöhr <oliver.alexander.stoehr@stud.fh-luebeck.de>
2017-09-07 16:51:21 +0200

    added content handling, added styling for titles

--- custom.js
+++ custom.js
@@ -8,10 +8,21 @@ function reload(loadingScreen) {
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
+			// getting Element where content should be placed
+			var contentElement = document.getElementById("content");
+			
+			//deleting possible content
+			while (contentElement.firstChild) {
+				contentElement.removeChild(contentElement.firstChild);
+			}
+			
 			// handling response
 			var data = JSON.parse(this.responseText);
-			// TODO handle data here and create charts
-						
+			// loop through array and create charts
+			for (i = 0; i < data.charts.length; i++) {
+				createChart(contentElement, i, data.titles[i], data.charts[i]);
+			}
+			
 			// finished loading
 			loadingScreen.dispatchEvent(loadEvent);
 		}
@@ -19,6 +30,36 @@ function reload(loadingScreen) {
 	
 	xmlhttp.open("GET", "data.php", true);
 	xmlhttp.send();
+}
+
+// function for creating new chart
+function createChart(parentElement, i, title, data) {
+	// creating nodes
+	var h1 = document.createElement("h2");
+	h1.innerHTML = title;
+	var canvas = document.createElement("canvas");
+	var canvasWrapper = document.createElement("div");
+	var col = document.createElement("div");
+	col.className = "col-md-4";
+	var card = document.createElement("div");
+	card.className = "card";
+	card.appendChild(h1);
+	canvasWrapper.appendChild(canvas);
+	card.appendChild(canvasWrapper);
+	col.appendChild(card);
+	// if is use to determine when a new row needs to be created
+	if (i%3 == 0) {
+		var row = document.createElement("div");
+		row.className = "row";
+		row.appendChild(col);
+		parentElement.appendChild(row);
+	} else {
+		parentElement.lastChild.appendChild(col);
+	}
+	
+	// creating chart
+	var myChart = new Chart(canvas, data);
+
 }
 
 // register EventListener & load content
